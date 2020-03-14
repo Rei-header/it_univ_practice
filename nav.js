d8db47d25002f38f2cf7f05b336d35d6539632c7
Rei-header <61345980+Rei-header@users.noreply.github.com>
2020-03-14 23:28:47 +0900

    first commit
    練習です。

---
+++ nav.js
@@ -0,0 +1,16 @@
+// 'use strict'
+
+// $('.burger-nav').click(function() {
+//   $('.nav-sp').fadeIn(0);
+
+//   $(this).addClass('open');
+//   $('.nav-sp').addClass('open');
+
+//   setTimeout(function() {
+//     $('nav-sp').removeClass('close');
+//   }, 0);
+//   setTimeout(function(){
+//     $(',nav-sp.open .nav li,.icon-close').fadeIn(0);
+//     $('.nav-sp .nav,.sacra-logo-sp,.nav-sns-sp,.icon-close,.nav-sublink-sp').addClass('fade-in');
+//   }, 600);
+// });
